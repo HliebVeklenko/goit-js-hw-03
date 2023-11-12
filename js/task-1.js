9477817bd9cd67fddd2b92f33fcf0234bea60e8b
Gleb Veklenko <glebveklenko59@gmail.com>
2023-11-12 11:45:13 +0200

    main
    done!

--- js/task-1.js
+++ js/task-1.js
@@ -1,14 +1,12 @@
 'use strict';
 
 function slugify(title) {
-    const doneTitle = title.toLowerCase().trim();
-    return titleWithLowerCase;
+    const doneTitle = title.toLowerCase().trim().split(" ");
+    const finalResultTitle = doneTitle.join("-");
+    return finalResultTitle;
 }
 
-// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-// console.log(slugify("English for developer")); // "english-for-developer"
-// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
-
-console.log(slugify("PoN"));
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
